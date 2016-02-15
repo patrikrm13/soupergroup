689c636229d42f32c012fae70ca7d3bf75911803
patrikrm13 <mahdi028a@gmail.com>
2016-02-15 23:39:01 +0330

    Update kickme.lua

--- plugins/kickme.lua
+++ plugins/kickme.lua
@@ -1,19 +1,19 @@
-local function kick_user(user_id, chat_id)
-  local chat = 'chat#id'..chat_id
+local function kick_user(user_id, channel_id)
+  local chat = 'channel#id'..channel_id
   local user = 'user#id'..user_id
-  chat_del_user(chat, user, function (data, success, result)
+  channel_del_user(chat, user, function (data, success, result)
     if success ~= 1 then
       send_msg(data.chat, 'Error while kicking user', ok_cb, nil)
     end
-  end, {chat=chat, user=user})
+  end, {channel=channel, user=user})
 end
 
 local function run (msg, matches)
   local user = msg.from.id
-  local chat = msg.to.id
+  local channel = msg.to.id
 
-  if msg.to.type ~= 'chat' then
-    return "Not a chat group!"
+  if msg.to.type ~= 'channel' then
+    return "Not a channel group!"
   elseif user == tostring(our_id) then
     --[[ A robot must protect its own existence as long as such protection does
     not conflict with the First or Second Laws. ]]--
@@ -21,7 +21,7 @@ local function run (msg, matches)
   elseif is_sudo(msg) then
     return "I won't kick an admin!"
   else
-    kick_user(user, chat)
+    kick_user(user, channel)
   end
 end
 
