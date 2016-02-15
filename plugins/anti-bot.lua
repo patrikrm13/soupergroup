7a48bc1baf6b5045147873c5c6d617aeb0fa467e
patrikrm13 <mahdi028a@gmail.com>
2016-02-15 23:46:00 +0330

    Update anti-bot.lua

--- plugins/anti-bot.lua
+++ plugins/anti-bot.lua
@@ -1,34 +1,34 @@
 
-local function isBotAllowed (userId, channelId)
-  local hash = 'anti-bot:allowed:'..channelId..':'..userId
+local function isBotAllowed (userId, chatId)
+  local hash = 'anti-bot:allowed:'..chatId..':'..userId
   local banned = redis:get(hash)
   return banned
 end
 
-local function allowBot (userId, channelId)
-  local hash = 'anti-bot:allowed:'..channelId..':'..userId
+local function allowBot (userId, chatId)
+  local hash = 'anti-bot:allowed:'..chatId..':'..userId
   redis:set(hash, true)
 end
 
-local function disallowBot (userId, channelId)
-  local hash = 'anti-bot:allowed:'..channelId..':'..userId
+local function disallowBot (userId, chatId)
+  local hash = 'anti-bot:allowed:'..chatId..':'..userId
   redis:del(hash)
 end
 
 -- Is anti-bot enabled on chat
-local function isAntiBotEnabled (channelId)
-  local hash = 'anti-bot:enabled:'..channelId
+local function isAntiBotEnabled (chatId)
+  local hash = 'anti-bot:enabled:'..chatId
   local enabled = redis:get(hash)
   return enabled
 end
 
-local function enableAntiBot (channelId)
-  local hash = 'anti-bot:enabled:'..channelId
+local function enableAntiBot (chatId)
+  local hash = 'anti-bot:enabled:'..chatId
   redis:set(hash, true)
 end
 
-local function disableAntiBot (channelId)
-  local hash = 'anti-bot:enabled:'..channelId
+local function disableAntiBot (chatId)
+  local hash = 'anti-bot:enabled:'..chatId
   redis:del(hash)
 end
 
@@ -39,8 +39,8 @@ local function isABot (user)
   return result == binFlagIsBot
 end
 
-local function kickUser(userId, channelId)
-  local chat = 'chat#id'..channelId
+local function kickUser(userId, chatId)
+  local chat = 'chat#id'..chatId
   local user = 'user#id'..userId
   chat_del_user(chat, user, function (data, success, result)
     if success ~= 1 then
@@ -51,40 +51,40 @@ end
 
 local function run (msg, matches)
   -- We wont return text if is a service msg
-  if matches[1] ~= 'channel_add_user' and matches[1] ~= 'channel_add_user_link' then
-    if msg.to.type ~= 'channel' then
+  if matches[1] ~= 'chat_add_user' and matches[1] ~= 'chat_add_user_link' then
+    if msg.to.type ~= 'chat' then
       return 'Anti-flood works only on channels'
     end
   end
 
   local chatId = msg.to.id
   if matches[1] == 'enable' then
-    enableAntiBot(channelId)
-    return 'Anti-bot enabled on this chat'
+    enableAntiBot(chatId)
+    return 'Anti-bot فعال'
   end
   if matches[1] == 'disable' then
-    disableAntiBot(channelId)
-    return 'Anti-bot disabled on this chat'
+    disableAntiBot(chatId)
+    return 'Anti-bot غیرفعال'
   end
   if matches[1] == 'allow' then
     local userId = matches[2]
-    allowBot(userId, channelId)
+    allowBot(userId, chatId)
     return 'Bot '..userId..' allowed'
   end
   if matches[1] == 'disallow' then
     local userId = matches[2]
-    disallowBot(userId, channelId)
+    disallowBot(userId, chatId)
     return 'Bot '..userId..' disallowed'
   end
-  if matches[1] == 'channel_add_user' or matches[1] == 'channel_add_user_link' then
+  if matches[1] == 'chat_add_user' or matches[1] == 'chat_add_user_link' then
     local user = msg.action.user or msg.from
     if isABot(user) then
       print('It\'s a bot!')
-      if isAntiBotEnabled(channelId) then
+      if isAntiBotEnabled(chatId) then
         print('Anti bot is enabled')
         local userId = user.id
-        if not isBotAllowed(userId, channelId) then
-          kickUser(userId, channelId)
+        if not isBotAllowed(userId, chatId) then
+          kickUser(userId, chatId)
         else
           print('This bot is allowed')
         end
@@ -106,8 +106,10 @@ return {
     '^!antibot (disallow) (%d+)$',
     '^!antibot (enable)$',
     '^!antibot (disable)$',
-    '^!!tgservice (channel_add_user)$',
-    '^!!tgservice (channel_add_user_link)$'
+    '^!lock bots (enable)$',
+    '^!lock bots (disable)$'
+    '^!!tgservice (chat_add_user)$',
+    '^!!tgservice (chat_add_user_link)$'
   },
   run = run
 }
